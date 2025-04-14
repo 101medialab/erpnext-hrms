91f2505136d45415b1d99bc6c69e155399f8084c
Derek Leung <derek.leung@hypebeast.com>
2025-04-14 14:38:54 +0800

    Update Dockerfile.

--- docker/erpnext_hrms_start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /home/frappe/frappe-bench
-
-bench clear-cache
-bench start
--- docker/hrms_setup_mariadb.sh
+++ docker/hrms_setup_mariadb.sh
@@ -2,13 +2,31 @@
 
 cd /home/frappe/frappe-bench
 
-bench set-mariadb-host mariadb
-bench new-site erpnext-hrms.local --admin-password admin --db-root-password 101medialab
+site_name="erpnext-hrms.local"
+db_name="erpnext_hrms_local"
+redis_host="redis://redis:6379"
 
-bench use erpnext-hrms.local
+if [ -d "/home/frappe/frappe-bench/sites/$site_name" ]; then
+    echo "$site_name already exists, skipping init"
+    bench use $site_name
+else
+    echo "Initializing $site_name..."
+    bench set-mariadb-host mariadb
+    bench set-redis-cache-host $redis_host
+    bench set-redis-queue-host $redis_host
+    bench set-redis-socketio-host $redis_host
 
-bench install-app hrms
-bench set-config developer_mode 1
-bench enable-scheduler
+    sed -i '/redis/d' ./Procfile
+    sed -i '/watch/d' ./Procfile
+
+    bench new-site $site_name --db-name $db_name --db-password 101medialab --admin-password admin --db-root-password 101medialab
+
+    bench use $site_name
+
+    bench install-app hrms
+    bench set-config developer_mode 1
+    bench enable-scheduler
+fi
+
 bench clear-cache
 bench start
