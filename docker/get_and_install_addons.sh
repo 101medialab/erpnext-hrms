48f5b781dada980d3d00aee22f30b624738d532d
Derek Leung <derek.leung@hypebeast.com>
2025-04-23 17:26:44 +0800

    New doctype and module

--- docker/get_and_install_addons.sh
+++ docker/get_and_install_addons.sh
@@ -4,8 +4,16 @@ cd /home/frappe/frappe-bench
 
 bench clear-cache
 
+rm -rf ./apps/erpnext_hrms_addons
+
 bench get-app erpnext_hrms_addons https://github.com/101medialab/erpnext-hrms-addons
 
+bench clear-cache
+
 bench install-app erpnext_hrms_addons
+
+bench migrate
+
+bench clear-cache
 
 bench restart
